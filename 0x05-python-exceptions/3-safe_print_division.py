ad4aebe86386bce785e09ca903fed2aa9fc0b96e
Samuel <samueleibo12@gmail.com>
2023-06-26 14:11:57 -0700

    modify file "3-safe_print_division.py"

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         operation = a / b
-    except Exception:
+    except (TypeError, ZeroDivisionError):
         operation = None
     finally:
         print("Inside result:{}".format(operation))
