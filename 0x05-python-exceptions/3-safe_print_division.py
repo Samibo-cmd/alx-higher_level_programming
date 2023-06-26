c3b7c9483fc9ce4b6d88557bb5ea09f92bef903a
Samuel <samueleibo12@gmail.com>
2023-06-26 14:07:51 -0700

    add new file "3-safe_print_division.py"

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        operation = a / b
+    except Exception:
+        operation = None
+    finally:
+        print("Inside result:{}".format(operation))
+    return (operation)
