aab7d47ff9619929d60138a1c03c4e22c140ef1a
Samuel <samueleibo12@gmail.com>
2023-06-28 04:29:00 -0700

    add new file "1-square.py"

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class named square"""
+
+
+class Square:
+    """This is a class of square"""
+    def __init__(self, size):
+        """Initialise new argument named size"""
+        self._size = size
