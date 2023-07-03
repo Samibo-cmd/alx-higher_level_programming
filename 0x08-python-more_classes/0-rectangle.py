d13684f375365d4ac20192d70c86c65cbeec8458
Samuel <samueleibo12@gmail.com>
2023-07-03 11:38:52 -0700

    create new project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+"""Define a class called rectangle"""
+class Rectangle:
+    """This is the class"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+Project: 0x08-python-more_classes
+
+Description: This project contains more exercises on python class and object
