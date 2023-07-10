aab116978dd6a5ab29a8e5a80ef4c2c0c20df97c
Samuel <samueleibo12@gmail.com>
2023-07-10 14:14:48 -0700

    add new file "6-base_geometry.py"

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents the basegeometry class."""
+
+    def area(self):
+        """an instance that raises execption of  non-implemention."""
+        raise Exception("area() is not implemented")
