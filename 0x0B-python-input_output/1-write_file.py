642830e45a41f05fd59b56775c28e6210b5cb162
Samuel <samueleibo12@gmail.com>
2023-07-11 12:04:11 -0700

    modify

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,6 @@
 """Defines a function that writes a string to a text file"""
 
 
-
 def write_file(filename="", text=""):
     """Represents the function"""
     with open(filename, "w", encoding="utf-8") as f:
