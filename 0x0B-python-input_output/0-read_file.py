384e64e491e773cb80ebfcf6751e4688e621e439
Samuel <samueleibo12@gmail.com>
2023-07-11 11:30:59 -0700

    modify

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """represents the function definition"""
-    with open("filename") as f:
+    with open('filename', 'r', encoding="utf-8") as f:
         for line in f:
             print(line, end="")
