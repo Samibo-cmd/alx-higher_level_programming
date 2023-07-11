bb271e553debb8c1736adb2c3fbc35250edbc4f8
Samuel <samueleibo12@gmail.com>
2023-07-11 12:11:04 -0700

    add new file "2-append_write.py"

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """Represents the function"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
