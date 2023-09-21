ec6cda66d26bd5802ea7cc252901646fb828f306
Samuel <samueleibo12@gmail.com>
2023-09-21 15:06:07 -0700

    modify file

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """prints the State object with the name passed as argument from the database"""
-import sy
+import sys
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
