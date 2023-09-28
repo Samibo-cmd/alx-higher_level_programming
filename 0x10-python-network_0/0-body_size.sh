6e84fbe928b6f50a87fb33a1b081179a15fb7a21
Samuel <samueleibo12@gmail.com>
2023-09-28 08:43:57 -0700

    aadd new project

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Takes in a URL, sends a request to that URL.
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,2 @@
+Project: 0x10-python-network_0
+Description: This project contains exercses on networking.
